4007277545e8224d51358435c270571b33569ab3
Pierre Wieser <p.wieser@trychlos.org>
2024-06-13 11:44:45 +0200

    Post-release minimal version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'pwix:ui-bootstrap5',
-    version: '2.0.2',
+    version: '2.0.3-rc',
     summary: 'A Meteor package which just includes Bootstrap 5 files and stylesheets',
     git: 'https://github.com/trychlos/pwix-ui-bootstrap5.git',
     documentation: 'README.md'
